34f85ffaa5cdddd9137ef2a83eea4f271090f600
hmunis <hadicanmuniss@gmail.com>
2024-07-10 04:24:26 +0300

    onboarding composable updated

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.littlelemon.composables.Onboarding
 import com.example.littlelemon.ui.theme.LittleLemonTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LittleLemonTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Hadican",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Onboarding()
             }
         }
     }
